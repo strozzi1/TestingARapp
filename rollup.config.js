a327d5a3299a8d1c94aa32bb45a53bce7c9a4a7e
asnow4u <asnow4u@gmail.com>
2020-01-24 14:32:35 -0800

    more testing

--- rollup.config.js
+++ rollup.config.js
@@ -6,12 +6,28 @@ var config = createDefaultConfig({
   // extension: ['.js', 'mjs']
 });
 
+function workbox(config) {
+  return {
+    name: 'workbox',
+    async writeBundle() {
+      var build = require('workbox-build');
+      const {count, size} = await build.generateSW(config);
+      console.log(count, size);
+    }
+  }
+}
+
 export default {
   ...config,
   plugins: [
       copy([
         { files: 'src/style.css', dest: 'dist' },
         { files: 'models/*.*', dest: 'dist/models'}
-      ], {verbose: false, watch: false}), ...config.plugins
-  ]
+      ], {verbose: false, watch: false}),
+      ...config.plugins,
+      workbox({
+        globDirectory: "dist",
+        swDest: "dist/sw.js",
+      }),
+  ],
 };
