7acd115d8c2405cac3a6af57b1390a02573d05f4
asnow4u <asnow4u@gmail.com>
2020-01-24 14:19:03 -0800

    testing

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,17 @@
-import { createDefaultConfig } from '@open-wc/building-rollup/modern-config';
+import copy from 'rollup-plugin-copy-glob';
+import { createDefaultConfig } from '@open-wc/building-rollup';
 
-export default createDefaultConfig({ input: './index.html' });
+var config = createDefaultConfig({
+  input: './index.html'
+  // extension: ['.js', 'mjs']
+});
+
+export default {
+  ...config,
+  plugins: [
+      copy([
+        { files: 'src/style.css', dest: 'dist' },
+        { files: 'models/*.*', dest: 'dist/models'}
+      ], {verbose: false, watch: false}), ...config.plugins
+  ]
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+//import * from 'three';
+//import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 console.log("WORKS");
